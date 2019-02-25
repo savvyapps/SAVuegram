e9a415f6c448ec8978a2c1be9dffd1e617fd5a69
Nathan Louie <nxlouie@umich.edu>
2019-02-25 17:59:25 -0500

    fixes that allow the project to work with the current firebase cloud firestore

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -17,12 +17,6 @@ const db = firebase.firestore()
 const auth = firebase.auth()
 const currentUser = auth.currentUser
 
-// date issue fix according to firebase
-const settings = {
-    timestampsInSnapshots: true
-}
-db.settings(settings)
-
 // firebase collections
 const usersCollection = db.collection('users')
 const postsCollection = db.collection('posts')
